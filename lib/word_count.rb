f0467c7b58ca28695392d185f4b4ab37756d500b
adaau <ada.au.cheng@gmail.com>
2015-10-26 10:00:51 +0800

    submit

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,18 @@
 class String
-  def unique_word_count
+
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
   end
-  
+
+  def unique_word_count
+    words.split.size
+  end
+
   def word_frequency_count
-  end  
+    frequency = Hash.new(0)
+    words.each { |word| frequency[word] += 1 }
+    frequency
+  end
 end
