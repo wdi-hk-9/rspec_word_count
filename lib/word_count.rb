3c36cd9a6fb65201592a0b76bf2be41312fbdf26
Fer Martin <fernandomartinsantana@gmail.com>
2015-10-26 09:33:08 +0800

    Solution for Assessment
    - Added :should to spec_helper.rb
    - Solution added to lib/word_count.rb

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,45 @@
 class String
   def unique_word_count
+    array_of_individual_words = words_from_string(self)
+    count_unique_words_in_array(array_of_individual_words)
   end
-  
+
   def word_frequency_count
-  end  
+    array_of_individual_words = words_from_string(self)
+    count_of_each_word(array_of_individual_words)
+  end
+
+  def words_from_string(original_string)
+    # 1. SPLIT
+    # Simpler (non-regex) method is to use .split() instead of .scan()
+    # However, .split() will not remove extranious punctuation.
+    # original_string.downcase.split()
+
+    # 2. SCAN
+    # This returns an array of individual words, stripped of punctiation and all lowercase
+    original_string.downcase.scan(/[\w']+/)
+
+    # http://rubular.com/
+    #
+    # Regular expression:
+    # [   #Character block start.
+    # ^   #Not these characters (letters, numbers).
+    # \w  #Word characters.
+    # \s  #Space characters.
+    # ]   #Character block end.
+  end
+
+  def count_unique_words_in_array(original_array_of_words)
+    original_array_of_words.uniq.count
+  end
+
+  def count_of_each_word(original_array_of_words)
+    counts = Hash.new(0)
+
+    original_array_of_words.each do |word|
+      counts[word] += 1
+    end
+
+    counts
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ RSpec.configure do |config|
   # config.color_enabled = true
   config.tty = true
   config.formatter = :documentation
+  config.expect_with(:rspec) { |c| c.syntax = :should }
 end
