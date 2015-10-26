f5e983703e59dbe2b33632406a25d3fe59df9c55
ianchoo <ianienchoo@gmail.com>
2015-10-26 10:01:55 +0800

    done

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,17 @@
 class String
   def unique_word_count
+    word_arr = self.split(/[\s.,#!$%&;:()]+/)
+    return word_arr.length
   end
-  
+
   def word_frequency_count
-  end  
+    word_arr = self.split(/[\s.,#!$%&;:()]+/)
+    freq = {}
+
+    word_arr.each do |word|
+      freq[word] =  freq[word] ? (freq[word] + 1) : 1
+    end
+
+    return freq
+  end
 end
